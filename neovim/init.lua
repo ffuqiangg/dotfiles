d37274bea6f358ad435c8553b1b81923f8b4bc30
ffuqiangg <ffuqiangg@gmail.com>
2023-02-07 16:02:03 +0800

    update

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,16 +1,9 @@
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-  vim.cmd [[packadd packer.nvim]]
-end
-
 require('packer').startup(function(use)
   -- Package manager
+  -- Install > https://github.com/wbthomason/packer.nvim#quickstart
   use 'wbthomason/packer.nvim'
 
+  -- Windows support > https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support
   use { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     run = function()
@@ -31,41 +24,9 @@ require('packer').startup(function(use)
   use 'navarasu/onedark.nvim' -- Theme inspired by Atom
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-
-  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
-  if has_plugins then
-    plugins(use)
-  end
-
-  if is_bootstrap then
-    require('packer').sync()
-  end
 end)
 
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
-end
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | silent! LspStop | silent! LspStart | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
-
 -- [[ Setting options ]]
--- See `:help vim.o`
 vim.opt.wrap = false
 vim.opt.linebreak = true
 
@@ -104,15 +65,15 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.wo.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.signcolumn = 'yes'
 
 -- Set colorscheme
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 vim.cmd [[colorscheme onedark]]
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.opt.completeopt = 'menuone,noselect'
 
 -- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
@@ -124,12 +85,6 @@ vim.g.maplocalleader = ' '
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for move corsor
-vim.keymap.set('n', '<C-H>', '^', { silent = true })
-vim.keymap.set('n', '<C-L>', '$', { silent = true })
-vim.keymap.set('n', '<C-J>', '<C-D>', { silent = true })
-vim.keymap.set('n', '<C-k>', '<C-U>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -222,16 +177,6 @@ require('nvim-treesitter.configs').setup {
     },
   },
 }
-
--- [[[Configure Onedark]]]
-require('onedark').setup {
-  transparent = true,
-  -- Change code style
-  code_style = {
-    comments = 'italic'
-  },
-}
-require('onedark').load()
 
 -- Configure autopairs
 require('nvim-autopairs').setup({
