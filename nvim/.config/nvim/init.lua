d2b6c2cadd4037f233902e735e12cf2d93185113
ffuqiangg <ffuqiangg@gmail.com>
2023-03-29 16:05:08 +0800

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -123,7 +123,7 @@ require('Comment').setup()
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'bash', 'yaml' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'yaml' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
