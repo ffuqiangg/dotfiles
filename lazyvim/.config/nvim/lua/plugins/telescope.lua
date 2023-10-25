66758f4ee4178634b512a8f3eede0a6bb94dd1a5
ffuqiangg <ffuqiangg@gmail.com>
2023-10-25 19:52:29 +0800

    update

--- lazyvim/.config/nvim/lua/plugins/telescope.lua
+++ lazyvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,16 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.49,
-          },
-          width = 0.9,
-          preview_cutoff = 100,
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.49,
         },
-      sorting_strategy = "ascending",
+        width = 0.9,
+        preview_cutoff = 100,
       },
+      sorting_strategy = "ascending",
     },
   },
 }
